ad5c3207e2dd32eb4472e1ee620f94f8d6dfb7f8
H29 <811416940@qq.com>
2026-06-30 20:03:04 +0800

    暂存

--- Mission/control_task/mission_planner.cpp
+++ Mission/control_task/mission_planner.cpp
@@ -7,6 +7,7 @@ extern "C"
     extern void start_spl06_task(void *argument);
     extern void init_task(void *argument);
     extern void pwm_task(void *argument);
+    extern void attitude_task(void *argument);
 
     void task_control(void const * argument)
     {
@@ -17,6 +18,8 @@ extern "C"
         // xTaskCreate(pwm_task,"pwm",512,nullptr,
         //             configMAX_PRIORITIES - 2, nullptr);
         xTaskCreate(start_spl06_task,"spl",128,nullptr,
+                    configMAX_PRIORITIES - 2, nullptr);
+        xTaskCreate(attitude_task,"attitude",256,nullptr,
                     configMAX_PRIORITIES - 2, nullptr);
 
         vTaskDelete(nullptr);
